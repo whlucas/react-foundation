9c26c573a1e6160fe58ecbc02c38982ede5ab42b
whlucas <1213372700@qq.com>
2019-03-15 09:09:41 +0800

    jsx1

---
+++ react-app-2/src/index.js
@@ -0,0 +1,22 @@
+// 引入
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let element = (
+    <div>
+        <h5>搜索热点</h5>
+        <span>换一换</span>
+    </div>
+    <ul>
+        <li>
+            <div></div>
+            <div></div>
+        </li>
+    </ul>
+)
+
+
+ReactDOM.render(element, document.getElementById('root'));
+
+
+
